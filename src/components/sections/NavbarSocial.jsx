f45143f09dc6598267639423e56d8f3e2f4a5c42
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-21 17:23:59 -0300

    fix: Navbar

--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -92,7 +92,7 @@ export default function NavbarSocial({ colorMode, mode }) {
     }
     // default
     return scrolling
-      ? "bg-opacity-100 shadow-lg transition-all duration-1000 bg-bgSectionDark border-b-[1px] border-primary"
+      ? "bg-opacity-100 shadow-lg transition-all duration-1000 bg-bgSectionDark border-b-[1px] border-white"
       : "transition-colors duration-1000";
   };
 
